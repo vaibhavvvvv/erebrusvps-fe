92bea05cc76dea5cbc1e58fdaac478ea148c7600
vaibhavvvvv <vaibhavng7@gmail.com>
2024-11-15 10:55:51 +0530

    fix build error

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
+import type { Configuration } from 'webpack'
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  webpack: (config: Configuration) => {
+    config.resolve = config.resolve || {}
+    config.resolve.alias = {
+      ...(config.resolve.alias || {}),
+      react: require.resolve('react'),
+    }
+    return config
+  },
+}
 
-export default nextConfig;
+export default nextConfig
